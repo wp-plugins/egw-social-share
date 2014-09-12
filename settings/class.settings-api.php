f62994118836c84013be79461b670afe28077d7d
proscriptsell <proscriptsell@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-12 21:02:08 +0000

    Credit added !
    git-svn-id: https://plugins.svn.wordpress.org/egw-social-share/trunk@988608 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- settings/class.settings-api.php
+++ settings/class.settings-api.php
@@ -589,13 +589,13 @@ class EGW_Settings_API {
 <div class="round-block">
 
 <div class="box"><img title="PSD to Wordpress" src="[live_url]/wp-content/themes/script-sell/images/ps.png" alt="" />
-<h3>PSD to Wordpress Service $350 </h3>
+<h3>PSD to Wordpress $350 </h3>
 I’m a freelance WordPress developer and web designer based in bangladesh, Dhaka. I started freelancing in 2007 and have worked for a wide range of personal clients and agencies.<br /><br />
 
 I can make your website look and function stunning using our PSD to Wordpress Theme Conversion and Customization Services.
 <br /><br />
 Contact Me: &nbsp; &nbsp;  skype: proscriptsell  &nbsp; &nbsp; & &nbsp; &nbsp; email:proscriptsell@gmail.com<br />
-Service Payment Method : <a href="www.skrill.com" target="_blank">www.skrill.com</a>
+Payment Method : <a href="http://www.skrill.com" target="_blank">www.skrill.com</a>
 <br /><br />
 </div>
 <h3>Please help me a bit  (Donate with Moneybookers)</h3>
